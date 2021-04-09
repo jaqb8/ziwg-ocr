177739b76f110004f0695bc035758e77f775906a
mgurski <mateuszgurski444@gmail.com>
2021-04-09 05:47:56 +0200

    Error handling done

--- client/react_app/src/App.tsx
+++ client/react_app/src/App.tsx
@@ -1,6 +1,7 @@
 import Decription from './components/Decription';
 import CameraButton from './components/CameraButton';
 import UploadButton from './components/UploadButton';
+import ReturnButton from './components/ReturnButton';
 import './styles.css';
 import { useReducer } from 'react';
 import MyContext from './helpers/context';
@@ -17,28 +18,33 @@ function App() {
 
   return (
     <MyContext.Provider value={{state, dispatch}}>
-      {state.status === 'start' && (
     <div className='page-size d-flex align-items-center justify-content-center'>
       <div className='body-size w-75 h-75 d-flex flex-column justify-content-between text-center align-items-center'>
         <div>
           <h3>Product Composition Recognizer</h3>
           <Decription className='lead description-size' />
         </div>
-        <CameraButton />
-        <UploadButton />
+        {state.status === 'start' && (
+        <>
+          <CameraButton />
+          <UploadButton />
+        </>)}
+        {state.status === 'image-sent' && (
+          <div>todo</div>
+        )}
+        {state.status === 'image-sending-failure' && (
+          <>
+          <div>{state.data}</div>
+            <ReturnButton/>
+          </>
+
+        )}
+        {state.status === 'additive-data-received' && (
+          <div>todo</div>
+        )}
         <div />
       </div>
-    </div>)}
-    {state.status === 'image-sent' && (
-      <div>todo</div>
-    )}
-    {state.status === 'image-sending-failure' && (
-      //TODO - Utworzenie komponentu wyświetlającego błędy
-      <div>{state.data}</div>
-    )}
-    {state.status === 'additive-data-received' && (
-      <div>todo</div>
-    )}
+    </div>
     </MyContext.Provider>
   );
 }
