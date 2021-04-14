c527eaeb42492be5e1da2e03652b2468b3b0efcc
Jakub Aniszewski <qba.anisz@gmail.com>
2021-04-14 22:06:12 +0200

    Text reccognition in endpoint

--- server/django_app/eadditives/views.py
+++ server/django_app/eadditives/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, views, status
 from rest_framework.response import Response
 from .models import Eadditives
 from .serializers import EadditivesSerializer
+from .ocr.ocr_service import recognize
 
 
 class EadditivesViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,7 +15,10 @@ class PhotoProcessView(viewsets.ViewSet):
     def create(self, request):
         request_fields = list(request.data)
         if request_fields and all([key == 'picture' for key in request_fields]):
-            return Response(status=status.HTTP_200_OK)
+            text = recognize(request.data['picture']).strip()
+            return Response(data={
+                'text': text
+            }, status=status.HTTP_200_OK)
         else:
             return Response(data={
                 'msg': 'Illegal fields provided.'
