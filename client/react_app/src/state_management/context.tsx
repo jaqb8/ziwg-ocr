c79333106258c4073926cf2c236eebc669d311aa
Wojtek <vopox07@gmail.com>
2021-05-07 15:33:52 +0200

    Move postImage into separate file to clean up context

--- client/react_app/src/state_management/context.tsx
+++ client/react_app/src/state_management/context.tsx
@@ -1,13 +1,14 @@
 import { Dispatch, createContext } from 'react';
-import axios from 'axios';
 import { useReducer } from 'react';
 import reducer from './reducer';
 import { Action, State } from './types';
+import postImage from './methods/postImage';
 
+// creating context
 type ContextState = {
   state: State;
   dispatch: Dispatch<Action>;
-  postImage: (files: FileList) => void;
+  postImage: (files: FileList, dispatch: Dispatch<Action>) => void;
 };
 
 const initialState = {
@@ -17,58 +18,18 @@ const initialState = {
 const initialContextState = {
   state: initialState,
   dispatch: () => {},
-  postImage: () => {},
+  postImage: postImage,
 };
 
 export const MyContext = createContext<ContextState>(initialContextState);
 
-type Props = {
+// creating provider, publishing context
+type ProviderProps = {
   children: React.ReactNode;
 };
 
-export const ContextProvider = ({ children }: Props) => {
+export const ContextProvider = ({ children }: ProviderProps) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const checkIfSupportedType = (fileList: FileList) => {
-    const types = ['image/png', 'image/jpeg'];
-
-    if (types.every((type) => fileList![0].type !== type)) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const postImage = async (fileList: FileList) => {
-    if (fileList!.length > 1) {
-      dispatch({
-        type: 'image-sending-failure',
-        error: 'Too many images selected',
-      });
-    } else if (!checkIfSupportedType(fileList!)) {
-      dispatch({
-        type: 'image-sending-failure',
-        error: 'Image type not supported',
-      });
-    } else {
-      dispatch({ type: 'image-sent' })
-      
-      var formData = new FormData();
-      formData.append('image', fileList![0]);
-      await axios
-        .post('http://127.0.0.1:8000/photo-process/', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        })
-        .then((response) =>
-          dispatch({ type: 'additive-data-received', response: response.data })
-        )
-        .catch((error) => {
-          dispatch({ type: 'image-sending-failure', error: error.message });
-        });
-    }
-  };
 
   return (
     <MyContext.Provider
